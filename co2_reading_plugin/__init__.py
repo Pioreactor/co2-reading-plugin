6a6a6a6d2fb65751ddcf6f01a314fccbb7f8f7ec
Kelly Tran <kelly.tran099@gmail.com>
2022-09-27 15:50:59 -0400

    commit

---
+++ co2_reading_plugin/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from co2_reading_plugin.co2-reading import click.co2-reading
---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name=cp2_reading_plugin",
+    version="0.0.1",
+    license="MIT",
+    description="Return a CO2 reading every set minutes from Adafruit CO2 sensors SCD30, SCD40 or SCD41.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author_email="kelly@pioreactor.com",
+    author="Kelly Tran",
+    url="https://github.com/Pioreactor/co2_reading_plugin",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={"pioreactor.plugins": "co2_reading_plugin = co2_reading_plugin"},
+)
