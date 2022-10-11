28e48044c7d2c6414ee5e79017b6f3449616e99a
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-10-11 14:16:42 -0400

    cleanup

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ setup(
     description="Return a CO2 reading (or all readings) every set minutes from Adafruit CO2 sensors SCD30, SCD40 or SCD41.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author_email="kelly@pioreactor.com",
-    author="Kelly Tran",
+    author_email="cam@pioreactor.com",
+    author="Kelly Tran, Cameron Davidson-Pilon",
     url="https://github.com/Pioreactor/co2_reading_plugin",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['install adafruit-circuitpython-scd30', 'install adafruit-circuitpython-scd4x']
+    install_requires=['install adafruit-circuitpython-scd30', 'install adafruit-circuitpython-scd4x'],
     entry_points={"pioreactor.plugins": "co2_reading_plugin = co2_reading_plugin"},
 )
