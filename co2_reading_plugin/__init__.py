cf337f85968d8de36117ba2fb860a86c94802e7c
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-10-11 14:40:23 -0400

    bump

--- co2_reading_plugin/__init__.py
+++ co2_reading_plugin/__init__.py
@@ -8,6 +8,7 @@ from pioreactor.background_jobs.leader.mqtt_to_db_streaming import produce_metad
 from pioreactor.background_jobs.leader.mqtt_to_db_streaming import register_source_to_sink
 from pioreactor.background_jobs.leader.mqtt_to_db_streaming import TopicToParserToTable
 from pioreactor.config import config
+from pioreactor.exc import HardwareNotFoundError
 from pioreactor.utils import timing
 from pioreactor.whoami import get_latest_experiment_name
 from pioreactor.whoami import get_unit_name
@@ -25,7 +26,7 @@ def parser(topic, payload) -> dict:
 
 register_source_to_sink(
     TopicToParserToTable(
-        "pioreactor/+/+/scd_reading/co2",
+        ["pioreactor/+/+/scd_reading/co2", "pioreactor/+/+/co2_reading/co2"],
         parser,
         "co2_readings",
     )
@@ -37,7 +38,7 @@ class SCDReading(BackgroundJob):
     job_name = "scd_reading"
 
     published_settings = {
-        "minutes_between_checks": {"datatype": "float", "unit": "min", "settable": True},
+        "interval": {"datatype": "float", "unit": "s", "settable": True},
         "co2": {"datatype": "float", "unit": "ppm", "settable": False},
         "temperature": {"datatype": "float", "unit": "°C", "settable": False},
         "relative_humidity": {"datatype": "float", "unit": "%rH", "settable": False},
@@ -47,14 +48,14 @@ class SCDReading(BackgroundJob):
         self,
         unit,
         experiment,
-        minutes_between_checks: float,
+        interval: float,
         skip_co2: bool = False,
         skip_temperature: bool = False,
         skip_relative_humidity: bool = False,
     ):
         super().__init__(unit=unit, experiment=experiment)
 
-        self.minutes_between_checks = minutes_between_checks
+        self.interval = interval
         self.skip_co2 = skip_co2
         self.skip_temperature = skip_temperature
         self.skip_relative_humidity = skip_relative_humidity
@@ -62,14 +63,22 @@ class SCDReading(BackgroundJob):
         i2c = board.I2C()
 
         if config.get("scd_config", "adafruit_sensor_type") == "scd30":
-            from adafruit_scd30 import SCD30
+            try:
+                from adafruit_scd30 import SCD30
 
-            self.scd = SCD30(i2c)
+                self.scd = SCD30(i2c)
+            except Exception:
+                self.logger.error("Is the SCD30 board attached to the Pioreactor HAT?")
+                raise HardwareNotFoundError("Is the SCD30 board attached to the Pioreactor HAT?")
         elif config.get("scd_config", "adafruit_sensor_type") == "scd4x":
-            from adafruit_scd4x import SCD4X
+            try:
+                from adafruit_scd4x import SCD4X
 
-            self.scd = SCD4X(i2c)
-            self.scd.start_periodic_measurement()
+                self.scd = SCD4X(i2c)
+                self.scd.start_periodic_measurement()
+            except Exception:
+                self.logger.error("Is the SCD4X board attached to the Pioreactor HAT?")
+                raise HardwareNotFoundError("Is the SCD4X board attached to the Pioreactor HAT?")
         else:
             self.logger.error(
                 "'adafruit_sensor_type' in [scd_config] not found. Did you mean one of 'scd30' or 'scd4x'?"
@@ -79,14 +88,14 @@ class SCDReading(BackgroundJob):
             )
 
         self.record_scd_timer = timing.RepeatedTimer(
-            self.minutes_between_checks * 60, self.record_scd, run_immediately=True
+            self.interval * 60, self.record_scd, run_immediately=True
         )
 
         self.record_scd_timer.start()
 
-    def set_minutes_between_checks(self, new_minutes_between_checks):
-        self.record_scd_timer.interval = new_minutes_between_checks * 60
-        self.minutes_between_checks = new_minutes_between_checks
+    def set_interval(self, new_interval):
+        self.record_scd_timer.interval = new_interval * 60
+        self.interval = new_interval
 
     def on_sleeping(self):
         # user pauses
@@ -117,18 +126,25 @@ class SCDReading(BackgroundJob):
             self.record_relative_humidity()
 
 
+class CO2Reading(SCDReading):
+    job_name = "co2_reading"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, skip_temperature=True, skip_relative_humidity=True, **kwargs)
+
+
 @click.command(name="scd_reading")
 @click.option(
-    "--minutes-between-checks",
-    default=config.getfloat("scd_config", "minutes_between_checks"),
+    "--interval",
+    default=config.getfloat("scd_config", "interval"),
     show_default=True,
 )
-def click_scd_reading(minutes_between_checks):
+def click_scd_reading(interval):
     """
     Start reading CO2, temperature, and humidity from the scd sensor.
     """
     job = SCDReading(
-        minutes_between_checks=minutes_between_checks,
+        interval=interval,
         unit=get_unit_name(),
         experiment=get_latest_experiment_name(),
     )
@@ -137,19 +153,17 @@ def click_scd_reading(minutes_between_checks):
 
 @click.command(name="co2_reading")
 @click.option(
-    "--minutes-between-checks",
-    default=config.getfloat("scd_config", "minutes_between_checks"),
+    "--interval",
+    default=config.getfloat("scd_config", "interval"),
     show_default=True,
 )
-def click_co2_reading(minutes_between_checks):
+def click_co2_reading(interval):
     """
     Only returns CO2 readings.
     """
-    job = SCDReading(
-        minutes_between_checks=minutes_between_checks,
+    job = CO2Reading(
+        interval=interval,
         unit=get_unit_name(),
         experiment=get_latest_experiment_name(),
-        skip_temperature=True,
-        skip_relative_humidity=True,
     )
     job.block_until_disconnected()
--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ from setuptools import setup
 
 setup(
     name="co2_reading_plugin",
-    version="0.0.3",
+    version="0.0.4",
     license="MIT",
-    description="Return a CO2 reading (or all readings) every set minutes from Adafruit CO2 sensors SCD30, SCD40 or SCD41.",
+    description="Return a CO2 reading every interval from an Adafruit CO2 sensors SCD30, SCD40 or SCD41.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author_email="cam@pioreactor.com",
